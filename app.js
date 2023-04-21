5ef04b76bcd02a0f8a3e0972af7b45de72413e37
Aiganysh26 <aiganyshbeisheeva7@gmail.com>
2023-04-21 21:56:11 +0200

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ function reverseStr(numbers){
   let result = ''
   for(let i = numbers.length - 1; i >= 0; i--){
     result += numbers[i]
+    
   }
-  return result
+  return result.split()
 }
 console.log(reverseStr(' фронтенд',' апрель',' урок'))
