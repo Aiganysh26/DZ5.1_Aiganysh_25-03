e7a720d09f92e3257b6ebed59e2423975912f532
Aiganysh26 <aiganyshbeisheeva7@gmail.com>
2023-04-22 13:05:29 +0200

    added map

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-function reverseStr(numbers){
-  let result = ''
-  for(let i = numbers.length - 1; i >= 0; i--){
-    result += numbers[i]
-    
-  }
+function reverseStr(...numbers){
+  let result = []
+  numbers.map(str => {
+    for(let i = str.length - 1; i >= 0; i--){
+      result += str[i]
+    }
+  })
   return result.split()
 }
 console.log(reverseStr(' фронтенд',' апрель',' урок'))
